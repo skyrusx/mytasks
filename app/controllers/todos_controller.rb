9a4cf86bf0eaaa87babc192ee1a38f9e24baf91c
Ruslan Fedotov <progruson@gmail.com>
2021-01-18 18:51:45 +0700

    Реализация TodosController

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,41 @@
+class TodosController < ApplicationController
+  before_action :set_project
+  before_action :set_project_todo, only: [:show, :update, :destroy]
+
+  def index
+    json_response @project.todos
+  end
+
+  def show
+    json_response @todo
+  end
+
+  def create
+    @project.todos.create! todo_params
+    json_response @project, :created
+  end
+
+  def update
+    @todo.update todo_params
+    head :no_content
+  end
+
+  def destroy
+    @todo.destroy
+    head :no_content
+  end
+
+  private
+
+  def todo_params
+    params.permit :text, :is_completed
+  end
+
+  def set_project
+    @project = Project.find params[:project_id]
+  end
+
+  def set_project_todo
+    @todo = @project.todos.find_by!(id: params[:id]) if @project
+  end
+end
