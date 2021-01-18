a06372c6966df1c4a2b3309170deac6a710ca812
Ruslan Fedotov <progruson@gmail.com>
2021-01-18 18:51:33 +0700

    Реализация ProjectsController

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,38 @@
 class ProjectsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :set_project, only: [:update, :destroy]
+
   def index
-    render json: {message: :ok}
+    @projects = Project.all
+    json_response @projects, :ok, {todos: {only: [:text, :is_completed]}}, [:created_at, :updated_at]
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.create! project_params
+    json_response @project, :created
+  end
+
+  def update
+    @project.update project_params
+    head :no_content
+  end
+
+  def destroy
+    @project.destroy
+    head :no_content
+  end
+
+  private
+
+  def project_params
+    params.permit :title, todos_attributes: [:text, :is_completed]
+  end
+
+  def set_project
+    @project = Project.find params[:id]
   end
 end
