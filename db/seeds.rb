f635257d28e598dfed8c42fbebec6a9eff6f3f6d
Ruslan Fedotov <progruson@gmail.com>
2021-01-18 19:46:01 +0700

    Сиды для проекта

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+FILE_PATH = [Rails.root, 'public', 'seeds.yml'].join('/')
+DEMO_DATA = YAML.load_file FILE_PATH
+
+def create_projects
+  DEMO_DATA['projects'].each do |project|
+    return if Project.where(title: project['title']).exists?
+    p = Project.create!(title: project['title'])
+
+    project['todos'].each do |todo|
+      todo_params = {text: todo['text'], project_id: p.id, is_completed: todo['isCompleted']}
+      p.todos.create! todo_params
+    end
+  end
+end
+
+create_projects
